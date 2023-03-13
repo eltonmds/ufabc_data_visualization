c9119603ad9cdc94fccd7f52a67faa887e52d309
ELTON MAURICIO DA SILVA <elton.silva@luizalabs.com>
2023-03-13 12:44:35 -0300

    pratice 3

---
+++ pratices/pratice_3/code/Q2.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import os
+
+print(os.getcwd())
+
+def format_df(df: pd.DataFrame, year: int) -> pd.DataFrame:
+    df = (
+        df[["Country", str(year)]]
+        .rename(columns={str(year): "hunger_index"})
+    )
+    df["hunger_index"] = df["hunger_index"].str.replace("<", "", regex=True)
+    df["year"] = year
+    
+    return df
+
+df = pd.read_csv("pratices/pratice_3/data/VIS_Pr_03_hungry_in_the_world.csv")
+
+
+df_chances = df[["Country", "Absolute change since 2014", "Percent change sice 2014"]].set_index("Country")
+
+df_2000 = format_df(df, 2000)
+df_2007 = format_df(df, 2007)
+df_2014 = format_df(df, 2014)
+df_2022 = format_df(df, 2022)
+
+concat_df = (
+    pd.concat(
+        [df_2000, df_2007, df_2014, df_2022]
+        )
+        .set_index("Country")
+)
+df_chances.to_csv("pratices/pratice_3/output_data/chances.csv")
+concat_df.to_csv("pratices/pratice_3/output_data/ghi.csv")
